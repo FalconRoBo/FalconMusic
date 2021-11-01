729747f4ccf30b955b18d26acfad36bd740f99e1
MadBoy <73926361+madboy482@users.noreply.github.com>
2021-11-01 10:44:20 +0530

    Initial commit

---
+++ falcon.sh
@@ -0,0 +1,14 @@
+echo "Cloning into FalconMusic ...."
+echo "+-+ +-+ +-+ +-+ +-+ +-+  +-+ +-+ +-+ +-+ +-+"
+echo "|F| |A| |L| |C| |O| |N|  |M| |U| |S| |I| |C|"
+echo "+-+ +-+ +-+ +-+ +-+ +-+  +-+ +-+ +-+ +-+ +-+"
+git clone https://github.com/FalconRoBo/FalconMusic.git /FalconMusic
+cd /FalconMusic
+echo "Starting Falcon VC Player for Telegram ...."
+echo "╔═══╗──╔╗────────────╔═╗╔═╗"
+echo "║╔══╝──║║────────────║║╚╝║║"
+echo "║╚══╦══╣║╔══╦══╦═╗───║╔╗╔╗╠╗╔╦══╦╦══╗"
+echo "║╔══╣╔╗║║║╔═╣╔╗║╔╗╗  ║║║║║║║║║══╬╣╔═╝"
+echo "║║──║╔╗║╚╣╚═╣╚╝║║║║  ║║║║║║╚╝╠══║║╚═╗"
+echo "╚╝──╚╝╚╩═╩══╩══╩╝╚╝  ╚╝╚╝╚╩══╩══╩╩══╝"
+python3.10 music.py
